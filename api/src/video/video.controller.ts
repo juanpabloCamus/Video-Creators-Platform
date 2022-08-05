d5d0504df58fb726427ddaa50e21dde89313a2fb
Juan Pablo <juanpablocamus28@gmail.com>
2022-08-05 17:11:33 -0300

    change video status endpoint

--- api/src/video/video.controller.ts
+++ api/src/video/video.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { CreateVideoDto } from './dto/video.dto';
 import { VideoService } from './video.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
@@ -8,10 +8,22 @@ export class VideoController {
     constructor(
         private videoService: VideoService
     ) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    getAllVideos(){
+        return this.videoService.getAllVideos()
+    }
     
     @UseGuards(JwtAuthGuard)
     @Post()
     createVideo(@Body() video:CreateVideoDto){
         return this.videoService.createVideo(video, video.idUser);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Put(':id')
+    changeVideoStatus(@Param() id){
+        return this.videoService.changeVideoStatus(id.id)
     }
 }
--- api/src/video/video.service.ts
+++ api/src/video/video.service.ts
@@ -11,13 +11,24 @@ export class VideoService {
         @InjectRepository(Video) private videoRepo: Repository<Video>,
         @InjectRepository(User) private userRepo: Repository<User>,
     ){}
+
+    async getAllVideos(){
+        return this.videoRepo.find({relations:['user']})
+    }
     
     async createVideo(video:CreateVideoDto, user:User){
         const {url, title, description, idUser} = video;
         if(!url || !title || !description || !idUser) throw new HttpException('MISSING_DATA', 400);
-
         const newVideo = this.videoRepo.create({...video,user:user});
         await this.videoRepo.save(newVideo);
         return newVideo;
+    }
+
+    async changeVideoStatus(id:number){
+        const video = await this.videoRepo.findOneBy({id:id})
+        if(video === null) throw new HttpException('NOT_FOUND', 404);
+        !video.public ? video.public = true : video.public = false
+        await this.videoRepo.save(video)
+        return video
     }
 }
